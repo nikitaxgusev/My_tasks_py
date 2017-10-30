c4d8aab18e2f114eba9609ad50d128a738f31cb6
nikitaxgusev <Elgolf@mail.ru>
2017-10-30 20:42:45 +0300

    Added changes.Added comments.
    1.Tried to add functions getName() to each object.
    2.Then use findWholeWord() ,which is based on regular expression, for finding a word with space around it in a string.
    3. Added MainFunction(), improve the algorithm of creating object.
    4. Commented the code
    
    Improvements.

--- main.py
+++ main.py
@@ -1,28 +1,59 @@
+import re # for regular expr.
+
+def findWholeWord(w):
+    """Regular expression for finding a word in a string.
+    Word should be around spaces."""
+
+    return re.compile(r'\b({0})\b'.format(w), flags=re.IGNORECASE).search
+
+
 class RichBoy(object):
+    """Rich Boy object.It has method type(), which return a needed string.
+    getName()- get a name from a object made by my hands"""
+
     def __init__(self):
-        self.counter=0
+        self.counter=0      # for the first phrase 'Hello'
+
+        # the common string for returning the comment.
+        #I just called it on a default mode
         self.s=("Hello")
 
     def Type(self,st):
+        """Method for returning a string into a chat.
+        The first phrase is always 'Hello' """
+
         if 0==self.counter:
             self.counter+=1
-            return self.s
+            return self.s   #return 'Hello'
 
-
-        if "buy" in st:
+        if "buy" in st:     #if in st is 'buy' return the string below "ofc,baby...."
             self.s=("Ofc,baby. I'm the richest boy in the world!")
             return self.s
-        else:
+        else:               #Else return another string
             self.s=("hey girls...")
             return self.s
+
+    def getName(self):
+        """Return the name of the object"""
+        return "RichBoy"
+
+
+#All comments, which are higher, can also correlate to another classes ,I mean about: Boy, Girl.
+#That's why dont comment them
+
 
 
 
 class Boy(object):
+    """Boy object.It has method type(), which return a needed string.
+        getName()- get a name from a object made by my hands"""
 
     def __init__(self):
          self.counter=0
          self.s=("Hello")
+
+    def getName(self):
+        return "Boy"
 
     def Type(self, st):
         #self.counter=0
@@ -40,9 +71,14 @@ class Boy(object):
 
 
 class Girl(object):
+    """Girl object.It has method type(), which return a needed string.
+            getName()- get a name from a object made by my hands"""
     def __init__(self):
         self.counter=0
         self.s=("Hello")
+
+    def getName(self):
+        return "Girl"
 
     def Type(self, st):
 
@@ -64,29 +100,48 @@ class Girl(object):
 
 
 
-def Get_OrderOfUsers():
-    Ls=[]
+def MainFunction():
+    """main function"""
+    c=0               #Counter for counting the size of a list of name users
+    Ls=[]                   #Empty list
+    gstr="Girl"         #The string is on default for print 137 line and etc.And redefine it in 117
+    bstr="Boy"
+    rstr="RichBoy"
+
     print("The order of user")
-    with open("OrderOfuser.txt","r") as f:
-       for line in f:
-           Ls.append(line.splitlines())
-    return Ls
+    with open("OrderOfuser.txt","r") as f: # read from a file
+        for line in f:
+            Ls.append(line.splitlines())    # splitlines helps me for taking a string from a file without '\n'
 
-L=[]
-L=Get_OrderOfUsers()
-g=Girl()
-b=Boy()
-r=RichBoy()
+            if  findWholeWord(gstr)(line):  #use r.expression
+                g = Girl()                  #if it's ok. Create and object
+                c+=1                  #size plus one
+                gstr=g.getName()            # getting Name for gstr
+
+            elif findWholeWord(bstr)(line):
+                b = Boy()
+                c+=1
+                bstr=b.getName()
+
+            elif findWholeWord(rstr)(line):
+                r = RichBoy()
+                c+=1
+                rstr=r.getName()
 
 
-s=("hey")
-bs=s
-for c in range(3):
-    s = g.Type(bs)
-    gs=s
-    print(L[1],":",s)
-    s = b.Type(s)
+    s=("hey")                       #The string for type method
     bs=s
-    print(L[0],":",s)
-    s=r.Type(gs)
-    print(L[2],":", s)
+    ListSize=c
+
+    for f in range(ListSize):       #till listsize under counter
+        s = g.Type(bs)
+        gs=s
+        print(gstr,":",s)           #print a comment
+        s = b.Type(s)
+        bs=s
+        print(bstr,":",s)
+        s=r.Type(gs)
+        print(rstr,":", s)
+
+
+MainFunction()
